ac8a5b10c85295d11a6c3ce275c0f6f526435c3b
Vincent <vigachet@gmail.com>
2024-11-22 11:29:38 +0100

    Update website titles

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -47,7 +47,7 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 const generateTitle: GenerateTitle<Post | Page> = ({ doc }) => {
-  return doc?.title ? `${doc.title} | Payload Website Template` : 'Payload Website Template'
+  return doc?.title ? `${doc.title} | Vincent Gachet | Portfolio` : 'Vincent Gachet | Portfolio'
 }
 
 const generateURL: GenerateURL<Post | Page> = ({ doc }) => {
